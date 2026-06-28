4f6259e737ccfad5815800df41f8ed46520f1db6
imanisul <ianisul768@gmail.com>
2026-06-28 10:35:12 +0530

    Database connected

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
-const app = require("./src/app");
+const app = require("./src/app.js");
+const connectDB = require("./src/config/db.js");
 
 const PORT = process.env.PORT;
+
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
