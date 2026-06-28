32ce1c153eb3414c440e76187aae2c0437d19e12
imanisul <ianisul768@gmail.com>
2026-06-28 12:00:32 +0530

    all routes created

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -3,10 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 
-const {createOrder} = require("../controllers/orderController.js");
+const {createOrder, getAllOrders, getOrderById, updateOrder, deleteOrder} = require("../controllers/orderController.js");
 
 
-route.post("/orders", createOrder);
+router.post("/orders", createOrder);
+
+router.get("/orders", getAllOrders);
+
+router.get("/orders/:id", getOrderById);
+
+router.put("/orders/:id", updateOrder);
+router.delete("/orders/:id", deleteOrder);
 
 module.exports = router;
 
