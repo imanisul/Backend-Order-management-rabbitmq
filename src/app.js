be1473d1c8bfd1d753247e6cf9c00371e256df73
imanisul <ianisul768@gmail.com>
2026-06-28 11:31:11 +0530

    registered route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
+const orderRoute = require("./routes/orderRoute.js");
+
 
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/v1", orderRoute);
 
 
 app.get("/", (req, res) => {
